00c68236844204b3404b7a55b1c059219aae2c97
Cevher Bakan <cevherbakan@gmail.com>
2025-08-27 19:26:02 +0300

    Created Sigma Project

---
+++ install.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+
+//Before running, we must create a database named "database" at MySql
+//For Create Table
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"database"
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+      var sql = "CREATE TABLE users ("+
+      "id int NOT NULL AUTO_INCREMENT PRIMARY KEY,"+
+        "firstName VARCHAR(255) NOT NULL,"+
+        "lastName VARCHAR(255) NOT NULL,"+
+         "birthday DATE NOT NULL,"+
+         "balance DECIMAL(5,2) NOT NULL,"+
+         "email VARCHAR(255) NOT NULL,"+
+         "reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+      conn.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
+
